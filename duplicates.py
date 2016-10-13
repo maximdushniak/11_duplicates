e427b53dd8caf45bfeaec41234039f2fc9241307
maximdushniak <maxim.dushniak@gmail.com>
2016-10-13 17:53:34 +0300

    done

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,48 @@
+import os
+import sys
+import argparse
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', nargs='?')
+
+    return parser
+
+
+def get_files(path):
+    files_size_dict = {}
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            filepath = root + '\\' + file
+            if files_size_dict.get((file, os.stat(filepath).st_size)):
+                files_size_dict[(file, os.stat(filepath).st_size)].append(filepath)
+            else:
+                files_size_dict[(file, os.stat(filepath).st_size)] = [filepath]
+
+    return files_size_dict
+
+
+def get_duplicates_files(files):
+    return [i for i in files.values() if len(i)>1]
 
 
 if __name__ == '__main__':
-    pass
+
+    parser = create_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+
+    if namespace.path:
+        path = namespace.filepath
+    else:
+        path = input('Insert path:')
+
+    files = get_files(path)
+
+    duplicates_files = get_duplicates_files(files)
+
+    if duplicates_files:
+        print('Found:')
+        print(duplicates_files)
+    else:
+        print('Not found.')
